74b063d7dc80f8a39db7da7707b46ff7a0726afc
sirajkhan8868 <sirajkhan8868447@gmail.com>
2024-10-01 03:15:36 -0700

    dashboard-integrate

---
+++ database/migrations/2024_09_30_122340_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user-id')->constrained();
+            $table->foreignId('catagory_id')->constrained();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->boolean('status')->default();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
